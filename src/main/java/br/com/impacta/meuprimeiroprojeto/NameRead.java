64c6ca8a2d01192e128c05d6314c757e00bd5cb1
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-12-21 20:27:20 -0300

    scanner e .equals

---
+++ src/main/java/br/com/impacta/meuprimeiroprojeto/NameRead.java
@@ -0,0 +1,15 @@
+package br.com.impacta.meuprimeiroprojeto;
+import java.util.Scanner;
+
+public class NameRead {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual o seu nome: ");
+        String nome = scanner.next();
+        if("cesar".equals(nome)){
+            System.out.print("Somo charás!!");
+        }else{
+            System.out.print("Prazer em conhecê-lo " + nome + ". Meu nome é Cesar");
+        }
+    }
+}
---
+++ src/main/java/br/com/impacta/meuprimeiroprojeto/StringManipulation.java
@@ -0,0 +1,12 @@
+package br.com.impacta.meuprimeiroprojeto;
+
+public class StringManipulation {
+    public static void main(String[] args) {
+        String nome = "Cesar";
+        String sobrenome = "August";
+        String nomeCompleto = nome + " " + sobrenome;
+        System.out.println(nomeCompleto);
+
+        //verifica se
+    }
+}
