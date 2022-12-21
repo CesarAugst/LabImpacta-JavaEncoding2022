0091475225d543049f564cad5af71e6a0df22669
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-12-20 22:37:24 -0300

    primeira aula

--- src/main/java/br/com/impacta/meuprimeiroprojeto/ProgramaUm.java
+++ src/main/java/br/com/impacta/meuprimeiroprojeto/ProgramaUm.java
@@ -1,7 +1,12 @@
 package br.com.impacta.meuprimeiroprojeto;
 
 public class ProgramaUm {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//declara numero
+        int numero1= 2000;
+        System.out.println("Meu numero " + numero1);
+        String nome = "Cesar";
+        System.out.println("Meu nome " + nome);
+
         System.out.format("My favorite number is %.3f.\n", 3.141592654);
 
         //part of a times table:
